6e3ec3adee78a296034c4cf555ee8fc5706aaf4c
Rick Lin <cl2545@en-ec-ath-02.coecis.cornell.edu>
2019-03-07 12:20:26 -0500

    forgot to add these to DB services

---
+++ ContactInformationDBServer.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import glob
+import sys
+import datetime
+import random
+import string
+
+sys.path.append('gen-py')
+
+from thrift           import Thrift
+from bank             import ContactInformationDB
+
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol  import TBinaryProtocol
+from thrift.server    import TServer
+
+SERVER_PORT = ('localhost', 19101)
+FIRST =   {'customerId':'firstName'}
+LAST =    {'customerId':'lastName'}
+HOME =    {'customerId':'homeNumber'}
+MOBILE =  {'customerId':'mobileNumber'}
+ADDRESS = {'customerId':'address'}
+
+
+class ContactInformationDBHandler:
+    def __init__(self):
+        self.log = {}
+
+    def ping(self):
+        print('ping()')
+
+    def retrieveCustomer(self, customerId):
+      contact = {}
+      contact['firstName']    = FIRST[customerId]   if customerId in FIRST.keys() else ''
+      contact['lastName']     = LAST[customerId]    if customerId in LAST.keys() else ''
+      contact['homeNumber']   = HOME[customerId]    if customerId in HOME.keys() else ''
+      contact['mobileNumber'] = MOBILE[customerId]  if customerId in MOBILE.keys() else ''
+      contact['address']      = ADDRESS[customerId] if customerId in ADDRESS.keys() else ''
+      return contact
+
+    def updateContactInformation(self, customerId, revisedInfo):
+      if 'firstName' in revisedInfo.keys():
+        FIRST[customerId]   = revisedInfo['firstName']
+      if 'lastName' in revisedInfo.keys():
+        LAST[customerId]    = revisedInfo['lastName'] 
+      if 'homeNumber' in revisedInfo.keys():
+        HOME[customerId]    = revisedInfo['homeNumber'] 
+      if 'mobileNumber' in revisedInfo.keys():
+        MOBILE[customerId]  = revisedInfo['mobileNumber'] 
+      if 'address' in revisedInfo.keys():
+        ADDRESS[customerId] = revisedInfo['address']
+
+      return True
+
+
+if __name__ == '__main__':
+    handler = ContactInformationDBHandler()
+    processor = ContactInformationDB.Processor(handler)
+    transport = TSocket.TServerSocket(host=SERVER_PORT[0], port=SERVER_PORT[1])
+    tfactory = TTransport.TBufferedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+
+    print('[' + SERVER_PORT[0] + ':' + str(SERVER_PORT[1]) + ']' + ' Starting the ContactInformationDBServer...')
+    server.serve()
+    print('[' + SERVER_PORT[0] + ':' + str(SERVER_PORT[1]) + ']' + ' ContactInformationDBServer done.')
---
+++ RegisteredProductsDBServer.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import glob
+import sys
+import datetime
+import random
+import string
+
+sys.path.append('gen-py')
+
+from thrift           import Thrift
+from bank             import RegisteredProductsDB
+
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol  import TBinaryProtocol
+from thrift.server    import TServer
+
+SERVER_PORT = ('localhost', 19102)
+CUSTOMER_CARDS = {'customerId':['number']}
+CUSTOMER_ACCOUNTS = {'customerId':['number']}
+
+class RegisteredProductsDBHandler:
+    def __init__(self):
+        self.log = {}
+
+    def ping(self):
+        print('ping()')
+
+    def getAccountNumbers(self, customerId):
+      accounts = CUSTOMER_ACCOUNTS[customerId]
+      return accounts
+
+    def getCardNumbers(self, customerId):
+      cards = CUSTOMER_CARDS[customerId]
+      return cards
+
+    def addCard(self, customerId):
+      global CUSTOMER_CARDS
+
+      newCardNumber = ''.join(random.choice(string.digits) for _ in range(16))
+      while any(newCardNumber in entry for entry in CUSTOMER_CARDS.values()):
+        newCardNumber = ''.join(random.choice(string.digits) for _ in range(16))
+      
+      if customerId not in CUSTOMER_CARDS.keys():
+        CUSTOMER_CARDS[customerId] = []
+
+      CUSTOMER_CARDS[customerId].append(newCardNumber)
+      print('['+SERVER_PORT[0]+':'+str(SERVER_PORT[1])+'] card created: ' + newCardNumber + ' for ' + customerId)
+
+      return newCardNumber
+
+    def addAccount(self, customerId):
+      global CUSTOMER_ACCOUNTS
+
+      newAccountNumber = ''.join(random.choice(string.digits) for _ in range(10))
+      while any(newAccountNumber in entry for entry in CUSTOMER_ACCOUNTS.values()):
+        newAccountNumber = ''.join(random.choice(string.digits) for _ in range(10))
+ 
+      if customerId not in CUSTOMER_ACCOUNTS.keys():
+        CUSTOMER_ACCOUNTS[customerId] = []
+
+      CUSTOMER_ACCOUNTS[customerId].append(newAccountNumber)
+      print('['+SERVER_PORT[0]+':'+str(SERVER_PORT[1])+'] account created: ' + newAccountNumber + ' for ' + customerId)
+      return newAccountNumber
+
+if __name__ == '__main__':
+    handler = RegisteredProductsDBHandler()
+    processor = RegisteredProductsDB.Processor(handler)
+    transport = TSocket.TServerSocket(host=SERVER_PORT[0], port=SERVER_PORT[1])
+    tfactory = TTransport.TBufferedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+
+    print('[' + SERVER_PORT[0] + ':' + str(SERVER_PORT[1]) + ']' + ' Starting the RegisteredProductsDBServer...')
+    server.serve()
+    print('[' + SERVER_PORT[0] + ':' + str(SERVER_PORT[1]) + ']' + ' RegisteredProductsDBServer done.')
